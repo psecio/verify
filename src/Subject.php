2cc48f334d95e9d19e686253222ad74c9d11bf92
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-21 16:00:05 -0600

    adding more methods and docblocks to Subject interface

--- src/Subject.php
+++ src/Subject.php
@@ -4,6 +4,27 @@ namespace Psecio\Verify;
 
 interface Subject
 {
+  /**
+   * Get the identifier for the subject (usually username)
+   * @return string identifier
+   */
   public function getIdentifier();
+
+  /**
+   * Get the credential value for the subject (like password)
+   * @return string Credential
+   */
   public function getCredential();
+
+  /**
+   * Get the groups list for the subject (set of strings)
+   * @return array Set of group names
+   */
+  public function getGroups();
+
+  /**
+   * Get the permissions list for the subject (set of strings)
+   * @return array Set of permission names
+   */
+  public function getPermissions();
 }
