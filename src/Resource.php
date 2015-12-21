415e76e5c17a4a71e528623a47cc72208bcdec8b
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-21 15:59:37 -0600

    adding a few missing pieces to Resource

--- src/Resource.php
+++ src/Resource.php
@@ -12,7 +12,17 @@ class Resource
     $uri = ($uri !== null) ?: $_SERVER['REQUEST_URI'];
     $this->setUri($uri);
 
-    $httpMethod = ($httpMethod == null) ?: $_SERVER['REQUEST_METHOD'];
+    $httpMethod = ($httpMethod !== null) ?: $_SERVER['REQUEST_METHOD'];
     $this->setHttpMethod($httpMethod);
+  }
+
+  public function setUri($uri)
+  {
+    $this->uri = $uri;
+  }
+
+  public function setHttpMethod($method)
+  {
+    $this->httpMethod = $method;
   }
 }
